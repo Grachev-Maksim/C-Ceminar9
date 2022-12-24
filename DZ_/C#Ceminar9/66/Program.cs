e23433782e28394dad0a578099fce6f140bfa6f2
Maks <you@example.com>
2022-12-24 19:59:24 +0300

    C#Ceminar9

---
+++ DZ_/C#Ceminar9/66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+//  которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+Console.WriteLine("Введите M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = SumNum(m, n);
+Console.WriteLine($"Сумма чисел от {m} до {n}  равна {sum}");
+
+int SumNum(int m, int n)
+{
+    if (m <= n)
+    {
+        return m + SumNum(m + 1, n);
+    }
+    else
+    {
+        return 0;
+    }
+}
---
+++ DZ_/C#Ceminar9/68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления функции
+// Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+Console.WriteLine("Введите M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = SumNum(m, n);
+Console.WriteLine($"вычисление функции Аккермана  {m} и {n}  равна {sum}");
+
+int SumNum(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else
+    {
+        if ((m != 0) && (n == 0))
+            return SumNum(m - 1, 1);
+        else
+            return SumNum(m - 1, SumNum(m, n - 1));
+    }
+}
