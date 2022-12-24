33d3d3e0c0af1bfb2daa4ee49887e389a3f92d7e
Maks <you@example.com>
2022-12-24 03:07:23 +0300

    C#Ceminar8

---
+++ DZ_/C#Ceminar8/58/Program.cs
@@ -0,0 +1,67 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу,
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j], 3}\t");
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = new Random().Next(-20, 21);
+}
+
+int[,] MultArr(int[,] array1, int[,] array2)
+{
+    int[,] res = new int[array1.GetLength(0), array2.GetLength(1)];
+    int curSum = 0;
+    for (int k = 0; k < array1.GetLength(0); k++)
+    {
+        for (int l = 0; l < array2.GetLength(1); l++)
+        {
+            for (int i = 0; i < array1.GetLength(1); i++)
+            {
+                curSum = curSum + array1[k, i] * array2[i, l];
+            }
+            res[k, l] = curSum;
+            curSum = 0;
+        }
+    }
+    return res;
+}
+
+Console.WriteLine("Введите количество строк матрицы А");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы А");
+int cols1 = Convert.ToInt32(Console.ReadLine());
+int[,] array1 = new int[rows1, cols1];
+FillArray(array1);
+PrintArray(array1);
+Console.WriteLine("Введите количество строк матрицы Б");
+int rows2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов матрицы Б");
+int cols2 = Convert.ToInt32(Console.ReadLine());
+int[,] array2 = new int[rows2, cols2];
+FillArray(array2);
+PrintArray(array2);
+if (cols1 != rows2)
+    Console.WriteLine("Матрицы не совместимы");
+else
+{
+    int[,] res = MultArr(array1, array2);
+    Console.WriteLine("Результат умножения матрицы А на матрицу Б ");
+    PrintArray(res);
+}
---
+++ DZ_/C#Ceminar8/62/Program.cs
@@ -0,0 +1,86 @@
+﻿// Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+Console.WriteLine("Введите размер массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[n, n];
+Spin(array);
+PrintArray(array);
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] < 10)
+                Console.Write($"0{array[i, j]}\t");
+            else
+                Console.Write($"{array[i, j], 2}\t");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+void Spin(int[,] arr)
+{
+    int proxod = 0;
+    int i = 0;
+    int j = 0;
+    int curr = arr.GetLength(0);
+    int tmp = 1;
+    while (tmp <= arr.GetLength(0) * arr.GetLength(0))
+    {
+        i = proxod;
+        j = proxod;
+        if ((i == proxod) && (j == proxod))
+        {
+            while (j < curr)
+            {
+                arr[i, j] = tmp;
+                tmp = tmp + 1;
+                j = j + 1;
+            }
+        }
+        if ((j == curr) && (i == proxod))
+        {
+            i = i + 1;
+            j = j - 1;
+            while (i < curr)
+            {
+                arr[i, j] = tmp;
+                tmp = tmp + 1;
+                i = i + 1;
+            }
+        }
+        if ((j == curr - 1) && (i == curr))
+        {
+            i = i - 1;
+            j = j - 1;
+            while (j >= proxod)
+            {
+                arr[i, j] = tmp;
+                tmp = tmp + 1;
+                j = j - 1;
+            }
+        }
+        if ((i == curr - 1) && (j == proxod - 1))
+        {
+            i = i - 1;
+            j = j + 1;
+            while (i > proxod)
+            {
+                arr[i, j] = tmp;
+                tmp = tmp + 1;
+                i = i - 1;
+            }
+        }
+        proxod = proxod + 1;
+        curr = curr - 1;
+    }
+}
